030e9979c170dc727dde74dc0b893d239bc9b694
Kanchan Jaiswal <91010576+kanchanCode@users.noreply.github.com>
2021-11-17 09:57:01 +0530

    addedGameboard

--- TicTacToeGame/TicTacToe.cs
+++ TicTacToeGame/TicTacToe.cs
@@ -6,5 +6,13 @@ namespace TicTacToeGame
 {
     class TicTacToe
     {
-    }
+        char[] Board = new char[10];
+        public void gameBase()
+        {
+           for(int i = 1; i <= 9; i++)
+          {
+                Board[i] = ' ';
+          }
+       }
+    }   
 }
